7934c81c855564b2982258aefc9fe8333a9f8b06
damarbagas-234 <muhammaddamar107@gmail.com>
2026-05-13 21:20:57 +0700

    Update Calculator class with basic arithmetic methods

--- lib/my_button_lib.dart
+++ lib/my_button_lib.dart
@@ -1,5 +1,22 @@
-/// A Calculator.
+/// A simple Calculator class.
 class Calculator {
+  /// Returns [a] + [b].
+  int add(int a, int b) => a + b;
+
+  /// Returns [a] - [b].
+  int subtract(int a, int b) => a - b;
+
+  /// Returns [a] * [b].
+  int multiply(int a, int b) => a * b;
+
+  /// Returns [a] / [b].
+  double divide(int a, int b) {
+    if (b == 0) {
+      throw ArgumentError('Cannot divide by zero');
+    }
+    return a / b;
+  }
+
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
